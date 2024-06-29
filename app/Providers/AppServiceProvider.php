16606b1d84a099feca271cb397b4c0a827ca23ae
Gede Dhanu Purnayasa <dhanuprys@gmail.com>
2024-06-29 22:15:02 +0800

    Add extra code for livewire

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production') || $this->app->environment('staging')) {
+            \URL::forceScheme('https');
+        }
+
         // Filament mass assignment
         Model::unguard();
     }
